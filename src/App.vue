c591b20daa2feb77dddd8e832a30cac7eb4af939
yxj0312 <yanxiajun@hotmail.com>
2021-10-11 22:34:40 +0200

    menu button

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,12 @@
     <div class="container mx-auto px-4">
       <h2 class="font-blod text-xl pt-2">Menu (DropDown)</h2>
       <Menu>
-        <MenuButton>More</MenuButton>
+        <MenuButton class="flex items-center bg-blue-500 text-white rounded-md px-4 py-2">        
+          <span>More</span>
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 ml-2" viewBox="0 0 20 20" fill="currentColor">
+              <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+            </svg>
+        </MenuButton>
         <MenuItems>
           <MenuItem v-slot="{ active }">
             <a :class='{ "bg-blue-500": active }' href="/account-settings">
