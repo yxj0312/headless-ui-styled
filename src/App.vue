f95c286114e9d41f69bd5c4c8f6ce7e0ca2c486d
yxj0312 <yanxiajun@hotmail.com>
2021-10-11 22:45:37 +0200

    install heroicons

--- src/App.vue
+++ src/App.vue
@@ -12,9 +12,7 @@
         <Menu>
           <MenuButton class="flex items-center bg-blue-500 text-white rounded-md px-4 py-2">        
             <span>More</span>
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 ml-2" viewBox="0 0 20 20" fill="currentColor">
-                <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-              </svg>
+            <ChevronDownIcon class="h-5 w-5 ml-2" />
           </MenuButton>
           <MenuItems>
             <MenuItem v-slot="{ active }">
@@ -44,6 +42,7 @@
 
 <script>
 import { Menu, MenuButton, MenuItems, MenuItem } from "@headlessui/vue";
+import { ChevronDownIcon } from '@heroicons/vue/solid'
 
   export default {
     components: {
