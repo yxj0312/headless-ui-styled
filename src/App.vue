c05388268675e258359b942e14f2bc96129e583c
yxj0312 <yanxiajun@hotmail.com>
2021-10-11 21:45:19 +0200

    add some h2

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,8 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <div class="min-h-screen bg-gray-200 text-gray-900">
-    <div class="container mx-auto">
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum vitae dolores maxime fuga veniam omnis natus autem molestiae nulla pariatur! Deserunt iusto a iure. Mollitia ipsa molestias esse laboriosam cupiditate.
+    <div class="container mx-auto px-4">
+      <h2 class="font-blod text-xl pt-2">Menu (DropDown)</h2>
     </div>
   </div>
 </template>
