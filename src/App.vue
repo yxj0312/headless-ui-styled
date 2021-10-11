5ba44e4f17b18cfec0c110cdcfd096ae70a4c825
yxj0312 <yanxiajun@hotmail.com>
2021-10-11 22:22:43 +0200

    add headless ui dropdown

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,31 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+
 </script>
 
 <template>
   <div class="min-h-screen bg-gray-200 text-gray-900">
     <div class="container mx-auto px-4">
       <h2 class="font-blod text-xl pt-2">Menu (DropDown)</h2>
+      <Menu>
+        <MenuButton>More</MenuButton>
+        <MenuItems>
+          <MenuItem v-slot="{ active }">
+            <a :class='{ "bg-blue-500": active }' href="/account-settings">
+              Account settings
+            </a>
+          </MenuItem>
+          <MenuItem v-slot="{ active }">
+            <a :class='{ "bg-blue-500": active }' href="/account-settings">
+              Documentation
+            </a>
+          </MenuItem>
+          <MenuItem disabled>
+            <span class="opacity-75">Invite a friend (coming soon!)</span>
+          </MenuItem>
+        </MenuItems>
+      </Menu>
     </div>
   </div>
 </template>
@@ -15,3 +33,17 @@ import HelloWorld from './components/HelloWorld.vue'
 <style>
 
 </style>
+
+<script>
+import { Menu, MenuButton, MenuItems, MenuItem } from "@headlessui/vue";
+
+  export default {
+    components: {
+      Menu,
+      MenuButton,
+      MenuItems,
+      MenuItem,
+    },
+  };
+</script>
+
