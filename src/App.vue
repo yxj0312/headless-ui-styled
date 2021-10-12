918d5d5fcefe089101f68f46adbd7dd46cef12f0
yxj0312 <yanxiajun@hotmail.com>
2021-10-12 20:03:07 +0200

    refactor first menu item

--- src/App.vue
+++ src/App.vue
@@ -14,21 +14,26 @@
             <span>More</span>
             <ChevronDownIcon class="h-5 w-5 ml-2" />
           </MenuButton>
-          <MenuItems class="absolute bg-white shadow-md rounded-lg flex flex-col focus:outline-none w-56 divide-y divide-gray-100 origin-top-left mt-2">
-            <MenuItem v-slot="{ active }">
-              <a :class='{ "bg-blue-500": active }' href="/account-settings">
-                Account settings
-              </a>
-            </MenuItem>
-            <MenuItem v-slot="{ active }">
-              <a :class='{ "bg-blue-500": active }' href="/account-settings">
-                Documentation
-              </a>
-            </MenuItem>
-            <MenuItem disabled>
-              <span class="opacity-75">Invite a friend (coming soon!)</span>
-            </MenuItem>
-          </MenuItems>
+          <div>
+            <MenuItems class="absolute bg-white shadow-md rounded-lg flex flex-col focus:outline-none w-56 divide-y divide-gray-100 origin-top-left mt-2">
+              <MenuItem v-slot="{ active }">
+                <button
+                  class="w-full flex items-center rounded-md px-2 py-2"
+                  :class='{ "bg-blue-500": active }'
+                >
+                  <span>Account settings</span>
+                </button>
+              </MenuItem>
+              <MenuItem v-slot="{ active }">
+                <a :class='{ "bg-blue-500": active }' href="/account-settings">
+                  Documentation
+                </a>
+              </MenuItem>
+              <MenuItem disabled>
+                <span class="opacity-75">Invite a friend (coming soon!)</span>
+              </MenuItem>
+            </MenuItems>
+          </div>
         </Menu>
       </div>
         
