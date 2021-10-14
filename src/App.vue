3e54ad7923a90d0da9179c1b6fa536ea440a0e9b
yxj0312 <yanxiajun@hotmail.com>
2021-10-14 23:43:51 +0200

    move to components

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <script setup>
+import Modal from './components/Modal.vue';
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 </script>
@@ -168,68 +169,14 @@
         <div class="mt-4">
           <h2 class="font-bold text-xl pt-2">Dialog (Modal)</h2>
           <div class="mt-4">
-            <button
+            <testModal/>
+            <!-- <button
               class="flex items-center bg-blue-500 text-white rounded-md px-4 py-2"
               @click="openModal"
             >
               <span>Open Modal</span>
               <ChevronDownIcon class="h-5 w-5 ml-2" />
-            </button>
-
-            <TransitionRoot appear :show="isOpen" as="template">
-              <Dialog as="div" @close="closeModal">
-                <div class="fixed inset-0 z-10 overflow-y-auto">
-                  <div class="min-h-screen px-4 text-center">
-                    <TransitionChild
-                      as="template"
-                      enter="duration-300 ease-out"
-                      enter-from="opacity-0"
-                      enter-to="opacity-100"
-                      leave="duration-200 ease-in"
-                      leave-from="opacity-100"
-                      leave-to="opacity-0"
-                    >
-                      <DialogOverlay class="fixed inset-0" />
-                    </TransitionChild>
-
-                    <span class="inline-block h-screen align-middle" aria-hidden="true">&#8203;</span>
-
-                    <TransitionChild
-                      as="template"
-                      enter="duration-300 ease-out"
-                      enter-from="opacity-0 scale-95"
-                      enter-to="opacity-100 scale-100"
-                      leave="duration-200 ease-in"
-                      leave-from="opacity-100 scale-100"
-                      leave-to="opacity-0 scale-95"
-                    >
-                      <div
-                        class="inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl"
-                      >
-                        <DialogTitle
-                          as="h3"
-                          class="text-lg font-medium leading-6 text-gray-900"
-                        >Payment successful</DialogTitle>
-                        <div class="mt-2">
-                          <p class="text-sm text-gray-500">
-                            Your payment has been successfully submitted. We’ve sent you
-                            an email with all of the details of your order.
-                          </p>
-                        </div>
-
-                        <div class="mt-4">
-                          <button
-                            type="button"
-                            class="inline-flex justify-center px-4 py-2 text-sm font-medium text-blue-900 bg-blue-100 border border-transparent rounded-md hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
-                            @click="closeModal"
-                          >Got it, thanks!</button>
-                        </div>
-                      </div>
-                    </TransitionChild>
-                  </div>
-                </div>
-              </Dialog>
-            </TransitionRoot>
+            </button> -->
           </div>
         </div>
       </div>
@@ -244,7 +191,7 @@ import {
 } from "@headlessui/vue";
 import { ChevronDownIcon } from "@heroicons/vue/solid";
 import { PencilIcon } from "@heroicons/vue/outline";
-
+import testModal from './components/Modal.vue'
 export default {
   components: {
     Menu,
@@ -258,6 +205,7 @@ export default {
     DialogTitle,
     ChevronDownIcon,
     PencilIcon,
+    Modal
   },
 
   setup() {
